c7536820c9714d77c3900c6effa0c65cb70264a8
subashini0690 <subashini0690@gmail.com>
2019-05-25 22:18:21 -0400

    Solution submission

--- app.js
+++ app.js
@@ -1,45 +1,53 @@
 (function () {
 'use strict';
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-LunchCheckController.$inject = ['$scope'];
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var toBuyList = this;
+  toBuyList.items = ShoppingListCheckOffService.getItemsToBuy();
 
-// <h1>Lunch Checker</h1>
-// <ol>Note for the reviwer:
-//   <li>I have not counted empty strings as a valid input. Hence, an iput like a,b,,,,,c is stil considered as 3 valid elements</li>
-//   <li>For a valid input, the message text will change color appropriately</li>
-//   <li>When the user is typing, no message will be displayed</li>
-// </ol>
-function LunchCheckController($scope) {
-  $scope.lunchMenu = "";
-  $scope.message = "";
-
-//clear the message when user is typing
-  $scope.clearMessage = function () {
-    $scope.message = "";
-    $scope.borderColor = {};
-  };
-  $scope.checkLunchMenu = function () {
-    let menuItems = $scope.lunchMenu.split(",");
-    //remove empty strings from the user input
-    menuItems = menuItems.filter(Boolean);
-    if (menuItems.length > 3) {
-      $scope.message = "Too Much!";
-      //change font and border color for the message
-      $scope.fontColor = {'color':'green'};
-      $scope.borderColor = {'border-color': 'green'}
-    } else if (menuItems.length === 0) {
-      $scope.message = "Please enter data first";
-      $scope.fontColor = {'color':'red'};
-      $scope.borderColor = {'border-color': 'red'}
-    } else if (menuItems.length <= 3) {
-      $scope.message = "Enjoy!";
-      $scope.fontColor = {'color':'green'};
-      $scope.borderColor = {'border-color': 'green'}
-    }
+  toBuyList.removeItem = function (itemName, itemQuantity, index) {
+    ShoppingListCheckOffService.removeItemAndAddToBought(itemName, itemQuantity, index);
   }
+}
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtList = this;
+  boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  var toBuyItems = [{ name: "Mandarins", quantity: 10 }, { name: "Water bottles", quantity: 10 },
+{ name: "Mangoes", quantity: 5 },{ name: "Bananas", quantity: 10 },{ name: "Strawberries", quantity: 20 }];
+  var boughtItems = [];
+
+  //Getters for items
+  service.getItemsToBuy = function () {
+    return toBuyItems;
+  };
+
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
+
+  //Fn to remove from toBuy list and add to bought list
+  service.removeItemAndAddToBought = function (itemName, quantity, index) {
+    toBuyItems.splice(index, 1);
+    boughtItems.push({
+      name: itemName,
+      quantity: quantity
+    });
+  };
+
 
 }
 
